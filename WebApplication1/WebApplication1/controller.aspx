83c4ae7c1775b5d30c99c3fd0b375377824ca489
osOmer <omer_sayem@yahoo.com>
2018-12-05 16:00:37 +0600

    Add files via upload

---
+++ WebApplication1/WebApplication1/controller.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="controller.aspx.cs" Inherits="WebApplication1.controller" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>
+        this is ctrl</p>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/controller.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class controller : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //load model
+            try
+            {
+
+                if (Request.QueryString.Get("ctrl").Equals("test"))
+                {
+                    Session.Add("val", "vAL sTRING");
+                    Server.TransferRequest("~/View/WebForm1.aspx");
+                }
+                else if (Request.QueryString.Get("ctrl").Equals("test2"))
+                {
+                    Server.TransferRequest("~/View/WebForm2.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                Server.TransferRequest("~/error.html");
+            }
+
+
+        }
+    }
+}
